b34661c43dd6f487f491c10c12b5c77df853c9cd
Siegfred Balona <siegblink@gmail.com>
2020-09-08 22:13:02 +0800

    Extract 'Navbar' from 'index' page as a separate component.

---
+++ components/Navbar.js
@@ -0,0 +1,19 @@
+import { Nav, Navbar } from 'react-bootstrap'
+
+export default function CustomNavbar() {
+  return (
+    <Navbar className='fj-navbar fj-nav-base' bg='transparent' expand='lg'>
+      <Navbar.Brand className='fj-navbar-brand'>
+        <a href='#'>Filip-Jerga</a>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='ml-auto'>
+          <Nav.Link className='fj-navbar-item fj-navbar-link' href='/'>
+            Home
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
