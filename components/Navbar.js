0b3c3ef0a3cca21f9da84283eb6ca638e97bb333
Siegfred Balona <siegblink@gmail.com>
2020-09-20 08:29:04 +0800

    Refactor 'CustomNavbar' component to use 'next.js' native 'Link' component.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,24 @@
+import Link from 'next/link'
 import { Nav, Navbar } from 'react-bootstrap'
 
 export default function CustomNavbar() {
   return (
     <Navbar className='fj-navbar fj-nav-base' bg='transparent' expand='lg'>
       <Navbar.Brand className='fj-navbar-brand'>
-        <a href='#'>Filip-Jerga</a>
+        <Link href='/'>
+          <a>Filip-Jerga</a>
+        </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
       <Navbar.Collapse id='basic-navbar-nav'>
         <Nav className='ml-auto'>
-          <Nav.Link className='fj-navbar-item fj-navbar-link' href='/'>
-            Home
-          </Nav.Link>
+          <Nav.Link
+            as={() => (
+              <Link href='/'>
+                <a className='fj-navbar-item fj-navbar-link'>Home</a>
+              </Link>
+            )}
+          />
         </Nav>
       </Navbar.Collapse>
     </Navbar>
