ab4d49cd7eee4afa91c899617579559f6921272a
Siegfred Balona <siegblink@gmail.com>
2020-09-24 09:14:17 +0800

    Add 'getServerSideProps' function in '[slug].js' dynamic page file.

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,15 +1,26 @@
-import { useRouter } from 'next/router'
 import PageLayout from 'components/PageLayout'
+import { getBlogBySlug } from 'lib/api'
 
-function BlogDetail() {
-  const { query } = useRouter()
+function BlogDetail({ blog }) {
   debugger
 
   return (
     <PageLayout>
-      <h1>Welcome to blog detail page - {query?.slug}</h1>
+      <h1>Welcome to blog detail page - {blog?.slug}</h1>
     </PageLayout>
   )
+}
+
+// This function is called during request time unlike 'getStaticProps'
+// Provides props to your 'BlogDetail'
+export async function getServerSideProps({ params }) {
+  const blog = await getBlogBySlug(params.slug)
+
+  return {
+    props: {
+      blog,
+    },
+  }
 }
 
 export default BlogDetail
