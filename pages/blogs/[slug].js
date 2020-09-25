3d3f3c5286b2b7c8f441b9adf3598faf4b5fc265
Siegfred Balona <siegblink@gmail.com>
2020-09-26 01:05:38 +0800

    Generate the 'slug' data dynamically by calling 'getAllBlogs' in 'getStaticPaths'.

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,9 +1,7 @@
 import PageLayout from 'components/PageLayout'
-import { getBlogBySlug } from 'lib/api'
+import { getBlogBySlug, getAllBlogs } from 'lib/api'
 
 function BlogDetail({ blog }) {
-  console.log("Displaying page")
-
   return (
     <PageLayout>
       <h1>Welcome to blog detail page - {blog?.slug}</h1>
@@ -14,8 +12,6 @@ function BlogDetail({ blog }) {
 // This function is called during request time unlike 'getStaticProps'
 // Provides props to your 'BlogDetail'
 export async function getStaticProps({ params }) {
-  console.log("Fetching blog by", params.slug)
-
   const blog = await getBlogBySlug(params.slug)
 
   return {
@@ -26,26 +22,10 @@ export async function getStaticProps({ params }) {
 }
 
 export async function getStaticPaths() {
-  console.log("Getting paths for every page.")
+  const blogs = await getAllBlogs()
 
   return {
-    paths: [
-      {
-        params: {
-          slug: 'my-second-blog',
-        },
-      },
-      {
-        params: {
-          slug: 'my-third-blog',
-        },
-      },
-      {
-        params: {
-          slug: 'my-first-blog',
-        },
-      },
-    ],
+    paths: blogs?.map((blog) => ({ params: { slug: blog.slug } })),
     fallback: false,
   }
 }
