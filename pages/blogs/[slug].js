15b85476079fdfa4a877e8e69bf2f750dd00221d
Siegfred Balona <siegblink@gmail.com>
2020-09-26 17:02:16 +0800

    Update 'BlogDetail' in '[slug].js' file to render 'BlogContent' component.

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,6 +1,7 @@
 import { Row, Col } from 'react-bootstrap'
 import PageLayout from 'components/PageLayout'
 import BlogHeader from 'components/BlogHeader'
+import BlogContent from 'components/BlogContent'
 import { getBlogBySlug, getAllBlogs } from 'lib/api'
 
 function BlogDetail({ blog }) {
@@ -10,16 +11,7 @@ function BlogDetail({ blog }) {
         <Col md={{ span: 10, offset: 1 }}>
           <BlogHeader {...blog} />
           <hr />
-          {/* Blog Content Here */}
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed to using 'Content here, content here', making it
-          look like readable English. Many desktop publishing packages and web
-          page editors now use Lorem Ipsum as their default model text, and a
-          search for 'lorem ipsum' will uncover many web sites still in their
-          infancy. Various versions have evolved over the years, sometimes by
-          accident, sometimes on purpose (injected humour and the like).
+          <BlogContent content={blog.content} />
         </Col>
       </Row>
     </PageLayout>
