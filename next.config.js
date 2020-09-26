0be7395411539f630637c42551ad599fecf2ee18
Siegfred Balona <siegblink@gmail.com>
2020-09-26 18:03:45 +0800

    Define 'next.js' specific configuration.

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// This configuration is needed in order to expose
+//   the environment variables in the browser
+module.exports = {
+  env: {
+    SANITY_DATASET_NAME: process.env.SANITY_DATASET_NAME,
+    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+  },
+}
