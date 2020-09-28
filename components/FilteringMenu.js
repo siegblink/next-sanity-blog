f48d34a8ef90ef38aa775349ecef1becf38d4e6b
Siegfred Balona <siegblink@gmail.com>
2020-09-28 09:09:30 +0800

    Add 'list icon' in 'FilteringMenu' component.

--- components/FilteringMenu.js
+++ components/FilteringMenu.js
@@ -1,11 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export default function FilteringMenu(props) {
   const { filter, onChange } = props
 
   return (
     <div className='filtering-menu mb-2'>
-      <div onClick={() => onChange('view', { list: +!filter.view.list })}>
-        List filter - {filter.view.list}
-      </div>
+      <FontAwesomeIcon
+        size='lg'
+        icon='list'
+        className='clickable hoverable'
+        onClick={() => onChange('view', { list: +!filter.view.list })}
+      />
     </div>
   )
 }
