374b3cf7778bd975a100a9c6714de8189d1cc0d8
Siegfred Balona <siegblink@gmail.com>
2020-09-28 09:08:44 +0800

    Add '@fortawesome/react-fontawesome' setup in '_app.js'.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBorderAll, faList } from '@fortawesome/free-solid-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'highlight.js/styles/night-owl.css'
 import 'styles/index.scss'
+
+library.add(faList, faBorderAll)
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
