f64e2a74fea70b5c1e6269070edbe102649f0158
Siegfred Balona <siegblink@gmail.com>
2020-09-29 23:07:23 +0800

    Assign dynamic data to the 'icon' prop of 'FontAwesomeIcon' in 'FilteringMenu' component.

--- components/FilteringMenu.js
+++ components/FilteringMenu.js
@@ -1,13 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+const listViewIcons = ['list', 'border-all']
+
 export default function FilteringMenu(props) {
   const { filter, onChange } = props
 
   return (
     <div className='filtering-menu mb-2'>
       <FontAwesomeIcon
-        size='lg'
-        icon='list'
+        size='2x'
+        icon={listViewIcons[filter.view.list]}
         className='clickable hoverable'
         onClick={() => onChange('view', { list: +!filter.view.list })}
       />
