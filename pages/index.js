1b8b460ae15bf0b654882a18cd2387d3079f227b
Siegfred Balona <siegblink@gmail.com>
2020-10-05 00:16:50 +0800

    Refactor 'index' page to make use of 'useGetBlogsPages' custom hook.

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,17 @@
 import { useState } from 'react'
-import { Row, Col } from 'react-bootstrap'
+import { Row } from 'react-bootstrap'
 import FilteringMenu from 'components/FilteringMenu'
-import CardListItem from 'components/CardListItem'
 import AuthorIntro from 'components/AuthorIntro'
 import PageLayout from 'components/PageLayout'
-import CardItem from 'components/CardItem'
 import { getAllBlogs } from 'lib/api'
-import { useGetBlogs } from 'actions'
+import { useGetBlogsPages } from 'actions/pagination'
 
-export default function Home(props) {
-  const { blogs: initialData } = props
-
+export default function Home({ blogs }) {
   const [filter, setFilter] = useState({ view: { list: 0 } })
 
-  const { data: blogs, error } = useGetBlogs(initialData)
+  const blogPages = useGetBlogsPages({ blogs, filter })
+
+  const { pages, isLoadingMore, isReachingEnd, loadMore } = blogPages
 
   return (
     <PageLayout>
@@ -24,47 +22,8 @@ export default function Home(props) {
           setFilter({ ...filter, [option]: value })
         }}
       />
-
       <hr />
-
-      {error ? (
-        <di>Error loading data.</di>
-      ) : !blogs ? (
-        <div>Loading...</div>
-      ) : (
-        <Row className='mb-5'>
-          {blogs.map((blog) =>
-            filter.view.list ? (
-              <Col key={`${blog.slug}-list`} md='9'>
-                <CardListItem
-                  date={blog.date}
-                  title={blog.title}
-                  author={blog.author}
-                  subtitle={blog.subtitle}
-                  link={{
-                    href: 'blogs/[slug]',
-                    as: `/blogs/${blog.slug}`,
-                  }}
-                />
-              </Col>
-            ) : (
-              <Col key={blog.slug} md='4'>
-                <CardItem
-                  date={blog.date}
-                  title={blog.title}
-                  author={blog.author}
-                  image={blog.coverImage}
-                  subtitle={blog.subtitle}
-                  link={{
-                    href: 'blogs/[slug]',
-                    as: `/blogs/${blog.slug}`,
-                  }}
-                />
-              </Col>
-            )
-          )}
-        </Row>
-      )}
+      <Row className='mb-5'>{pages}</Row>
     </PageLayout>
   )
 }
